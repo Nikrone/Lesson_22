f3605b1591afc01eee1307d97b55d9d3ed9f420d
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-10-11 20:11:16 +0300

    added part 1 Lesson-22

--- Lesson_22/ViewController.swift
+++ Lesson_22/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+//        прекращение обновление локации
+//        locationManager.stopUpdatingLocation()
     }
-
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let status = manager.authorizationStatus
+        switch status {
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            break
+        case .restricted:
+            break
+        }
+    }
+}
