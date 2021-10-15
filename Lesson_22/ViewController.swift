3e5054fe0e39ca4ce21590e306fbff04ebb62ca5
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-10-15 14:05:56 +0300

    added video

--- Lesson_22/ViewController.swift
+++ Lesson_22/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var videoView: UIView!
 
-    var audioPlayer = AVAudioPlayer()
+//    var audioPlayer = AVAudioPlayer()
     
     var videoPlayer = AVPlayer()
     
@@ -20,44 +20,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        ЗАПУСК АУДИО!
-        guard let audioURL = Bundle.main.url(forResource: "dorofeeva_-_nevesta", withExtension: "mp3") else {
-            return
-        }
-        audioPlayer = (try? AVAudioPlayer(contentsOf:audioURL)) ?? AVAudioPlayer()
-        audioPlayer.volume = 0
+//        guard let audioURL = Bundle.main.url(forResource: "dorofeeva_-_nevesta", withExtension: "mp3") else {
+//            return
+//        }
+//        audioPlayer = (try? AVAudioPlayer(contentsOf:audioURL)) ?? AVAudioPlayer()
+//        audioPlayer.volume = 0
         
         
 //        ЗАПУСК ВИДЕО!
-//        guard let url = Bundle.main.url(forResource: "", withExtension: "") else {
-//            return
-//        }
-//        videoPlayer = AVPlayer(url: url)
-//
-//        let playerLayer = AVPlayerLayer(player: videoPlayer)
-//        playerLayer.frame = videoView.bounds
-//        videoView.layer.addSublayer(playerLayer)
-//
-//
-//        videoPlayer.play()
+        guard let url = Bundle.main.url(forResource: "OneRepublic - Love Runs Out (Official Music Video)", withExtension: "mp4") else {
+            return
+        }
+        videoPlayer = AVPlayer(url: url)
+
+        let playerLayer = AVPlayerLayer(player: videoPlayer)
+        playerLayer.frame = videoView.bounds
+        videoView.layer.addSublayer(playerLayer)
+
+
+        videoPlayer.play()
     }
     
-    @IBAction func startButtonPressed() {
-        audioPlayer.play()
-        audioPlayer.volume += 0.1
-
-    }
-
-    @IBAction func stopButtonPressed() {
-        audioPlayer.pause()
-    }
+//    @IBAction func startButtonPressed() {
+//        audioPlayer.play()
+//        audioPlayer.volume += 0.1
+//
+//    }
+//
+//    @IBAction func stopButtonPressed() {
+//        audioPlayer.pause()
+//    }
     
     @IBAction func show() {
-//        let controller = AVPlayerViewController()
-//        controller.player = videoPlayer
-//        show(controller, sender: nil)
+        let controller = AVPlayerViewController()
+        controller.player = videoPlayer
+        show(controller, sender: nil)
         
-        let url  = URL(string: "applicationType://")!
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+//        Переход в другое приложение!
+//        let url  = URL(string: "applicationType://")!
+//        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
 
 
